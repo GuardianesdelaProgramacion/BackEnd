d213f14734eebfb8192f28bee6540801b70c4b31
EmilianoEP <estrapa.emi@gmail.com>
2022-08-17 00:47:58 -0500

    Mod: contactoentity

--- src/main/java/com/musarana/app/service/IContactoService.java
+++ src/main/java/com/musarana/app/service/IContactoService.java
@@ -1,5 +1,12 @@
 package com.musarana.app.service;
 
-public interface IContactoService {
+import java.util.List;
 
+import com.musarana.app.entity.Contacto;
+
+public interface IContactoService {
+	public List<Contacto> findAllcontacto();
+	public Contacto saveContacto(Contacto contacto);
+	public Contacto deleteContactoById(Long id);
+	public Contacto findContactobyId(Long id);
 }
